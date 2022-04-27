02846b6258f2821ec095f5b3866bb5b6e12a439e
kimsunmoo <tjsan199813@gmail.com>
2022-04-28 00:09:20 +0900

    path add '/'

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 PROJECT_HOME = os.getcwd()
 UTAGGER_HOME = os.path.join(PROJECT_HOME, 'utagger/bin')
 
-IMAGE_PATH = os.path.join(PROJECT_HOME, 'image')
-ARTICLE_PATH = os.path.join(PROJECT_HOME, 'article')
-KEYWORD_PATH = os.path.join(PROJECT_HOME, 'key')
-AUDIO_PATH = os.path.join(PROJECT_HOME, 'audio')
-FONT_PATH = os.path.join(PROJECT_HOME, 'font')
+IMAGE_PATH = os.path.join(PROJECT_HOME, 'image/')
+ARTICLE_PATH = os.path.join(PROJECT_HOME, 'article/')
+KEYWORD_PATH = os.path.join(PROJECT_HOME, 'key/')
+AUDIO_PATH = os.path.join(PROJECT_HOME, 'audio/')
+FONT_PATH = os.path.join(PROJECT_HOME, 'font/')
 
 GLOW_CHECKPOINT = "/home/jovyan/glow_cp/glowtts-KSS/glowtts-v2-March-29-2022_04+50AM-3aa165a/best_model.pth.tar"
 HIFI_CHECKPOINT = "/home/jovyan/hifi_cp/hifigan-KSS/hifigan-v2-March-29-2022_07+27AM-3aa165a/best_model.pth.tar"
